0cf7b1f44a89178e0e33aedc88afb51cfedd9b28
itsRhythm <rhythmfahim@gmail.com>
2023-11-03 10:41:27 +0600

    MIS455 assignment 2

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let outputEl = document.getElementById("output-field")
 let submitBtn = document.getElementById("submit-btn")
 
 submitBtn.addEventListener("click", function(){
+    event.preventDefault()
     document.getElementById("form-txt").innerHTML = `<h2>${person.value}</h2>
     <p>Your reservation request for <b>${category.value}</b> has been accepted</p>
     <p>You will recieve a confirmation email at <i> ${email.value}</i></p>`
